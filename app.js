a433daacae4fd6a18584a1256018b85c5e99c8d6
Khai Nguyen <khaib2111847@student.ctu.edu.vn>
2024-11-17 21:08:00 +0700

    add middlewares

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const routes = require('./app/routes'); // Kết nối tới file index.js trong thư mục routes
+const loggerMiddleware = require('./app/middlewares/loggerMiddleware');
+const authMiddleware = require('./app/middlewares/authMiddleware');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(loggerMiddleware); // Ghi log tất cả các request
 
 // Routes
 app.use('/api', routes);
+app.use('/api/public', routes);
+app.use('/api/secure', authMiddleware, routes);
 
 module.exports = app; // Xuất module để sử dụng trong server.js
--- app/routes/docgiaRoute.js
+++ app/routes/docgiaRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getAllDocGia, createDocGia } = require('../controllers/docGiaController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 const router = express.Router();
 
-router.get('/', getAllDocGia); // Lấy danh sách độc giả
+router.get('/', authMiddleware, getAllDocGia); // Lấy danh sách độc giả
 router.post('/', createDocGia); // Tạo mới độc giả
 
 module.exports = router;
